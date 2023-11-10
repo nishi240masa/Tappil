1b0e42f8b8677cbd959bc405233a7ad7f834f516
nishi240masa <masa2401106@gmail.com>
2023-11-10 18:23:40 +0900

    add:apiファイルの作成

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
