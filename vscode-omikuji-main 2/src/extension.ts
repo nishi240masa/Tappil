666f881bb471c5163fbf4efad87e493a13c18519
nishi240masa <masa2401106@gmail.com>
2023-11-11 14:47:11 +0900

    add:サーバーに送る為の設定を追加

--- vscode-omikuji-main 2/src/extension.ts
+++ vscode-omikuji-main 2/src/extension.ts
@@ -1,4 +1,13 @@
 import * as vscode from 'vscode';
+import * as https from "https";
+import * as http from "http";
+
+
+const data = JSON.stringify({
+    text: "text",
+  });
+
+const url = "http://localhost:3000/api/data";
 
 let keyCount = 0;
 let BackCount = 0;
@@ -6,6 +15,7 @@ let enter = 0;
 let cursorTimer: NodeJS.Timeout | undefined = undefined;
 let totalCursorTimeInMilliseconds = 0;
 let isVsCodeActive = true; // VSCodeウィンドウがアクティブかどうかを保持する変数
+
 
 const activeEditor = vscode.window.activeTextEditor;
 
@@ -58,6 +68,24 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('vscode-Keys.showKeyCount', () => {
             const seconds = (totalCursorTimeInMilliseconds).toFixed(2); // カーソルがエディタ上にあった時間を小数点以下2桁まで表示
             vscode.window.showInformationMessage(`キーを ${keyCount} 回押しました。エンターキーを ${enter} 回押しました。バックスペースキーを ${BackCount} 回押しました。マウスカーソルがエディタ上にあった時間: ${seconds} 秒`);
+
+
+
+            const datas = {
+                "name": "test", 
+                  "keycount": keyCount, 
+                  "entercount": enter,
+                   "backcount": BackCount, 
+                   "seconds": seconds,
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              };    
+            const request = http.request(url,datas, response => {
+                console.log(`statusCode: ${response.statusCode}`)
+              
+              })
+
 		}
         )
     }
