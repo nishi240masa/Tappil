a5e47a1cd50512b5f04b5454ef89fe3088761156
nishi240masa <masa2401106@gmail.com>
2023-11-15 22:48:16 +0900

    aa

--- api/svg.cjs
+++ api/svg.cjs
@@ -4,7 +4,8 @@ const SVG = require('svg-canvas');
 
 exports.svgData = function (req, user) {
   console.log("test_svg");
-  console.log(req);
+  console.log(req.body);
+  
     let sco = [];
     let keyco = [];
     let sec = [];
