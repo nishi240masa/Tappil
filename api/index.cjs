56b7caa53e7c3c0f0c57ff8812e2728d78db8094
nishi240masa <masa2401106@gmail.com>
2023-11-15 14:40:44 +0900

    kaka

--- api/index.cjs
+++ api/index.cjs
@@ -69,7 +69,7 @@ app.use(bodyParser.json());
 // postされたデータを受け取る設定
 app.post('/api/data', (req, res) => {
 
-    console.log("test"+req.body);
+    console.log(req.body);
 
     let name_data = req.body.name;
 
