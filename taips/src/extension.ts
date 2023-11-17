2a3987ef01b268e13f73dfba0eba3b18294e9738
hiroto09 <h16100929@icloud.com>
2023-11-18 02:06:58 +0900

    Update keybindings and prompt for name in extension

--- taips/src/extension.ts
+++ taips/src/extension.ts
@@ -18,11 +18,7 @@ async function promptForName(name: string | undefined) {
         vscode.window.showWarningMessage('No name entered.');
     }
     //nameに入力された文字列入ってる
-
-
 }
-
-
 
 const data = JSON.stringify({
     text: "text",
@@ -44,12 +40,14 @@ let totalCursorTimeInMilliseconds = 0;
 let isVsCodeActive = true; // VSCodeウィンドウがアクティブかどうかを保持する変数
 let name: string | undefined = undefined;
 let hasEnteredIfBlock = false;
-
+let activ=false;
 const activeEditor = vscode.window.activeTextEditor;
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
-        
+
+
+    // ここからは、、VSCodeのコマンドpromptForNameの設定
         vscode.commands.registerCommand('extension.promptForName', async () => {
           /*  const activeEditor = vscode.window.activeTextEditor;
             if (activeEditor) {
@@ -58,10 +56,8 @@ export function activate(context: vscode.ExtensionContext) {
             }else{
                 vscode.window.showInformationMessage('エディタ開かない');
             }*/
-
-
-  
-            if(name === undefined){
+ 
+        if(name === undefined){
             const result = await vscode.window.showInputBox({
                 prompt: 'Please enter your name',
                 placeHolder: 'Your Name'
@@ -78,9 +74,10 @@ export function activate(context: vscode.ExtensionContext) {
         }),
 
 
+    // ここからは、、VSCodeのコマンドStartの設定
         vscode.commands.registerCommand('vscode-Keys.Start', () => {
             console.log(name);
-
+            activ=true; 
             if (name !== undefined) {
             vscode.window.showInformationMessage(`たいぷず開始！！`);
     
@@ -94,7 +91,9 @@ export function activate(context: vscode.ExtensionContext) {
                     }
                 }
             });
-            // カーソルがエディタ上にある時間を計測する
+
+
+    // カーソルがエディタ上にある時間を計測する
             vscode.window.onDidChangeTextEditorSelection((e) => {
                 if (e.textEditor && isVsCodeActive) {
                     if (!cursorTimer) {
@@ -107,6 +106,8 @@ export function activate(context: vscode.ExtensionContext) {
                 }
             });
 
+
+         //カーソルの処理
             vscode.window.onDidChangeActiveTextEditor(() => {
                 if (cursorTimer && !isVsCodeActive) {
                     clearInterval(cursorTimer);
@@ -114,12 +115,12 @@ export function activate(context: vscode.ExtensionContext) {
                 }
             });
         
-              
-        
+
+
+         // キーの取得の処理
             const onDidChangeTextDocument = vscode.workspace.onDidChangeTextDocument((event) => {
                 
-                const text = event.contentChanges[0]?.text;
-                
+            const text = event.contentChanges[0]?.text;
 
                 if (text === 'a' || text === 'b' || text === 'c' || text === 'd' || text === 'e' || text === 'f' || text === 'g' || text === 'h' || text === 'i' || text === 'j' || text === 'k' || text === 'l' || text === 'm' || text === 'n' || text === 'o' || text === 'p' || text === 'q' || text === 'r' || text === 's' || text === 't' || text === 'u' || text === 'v' || text === 'w' || text === 'x' || text === 'y' || text === 'z' || text === 'A' || text === 'B' || text === 'C' || text === 'D' || text === 'E' || text === 'F' || text === 'G' || text === 'H' || text === 'I' || text === 'J' || text === 'K' || text === 'L' || text === 'M' || text === 'N' || text === 'O' || text === 'P' || text === 'Q' || text === 'R' || text === 'S' || text === 'T' || text === 'U' || text === 'V' || text === 'W' || text === 'X' || text === 'Y' || text === 'Z' || text === '1' || text === '2' || text === '3' || text === '4' || text === '5' || text === '6' || text === '7' || text === '8' || text === '9' || text === '0') {
                     keyCount++; 
@@ -130,59 +131,18 @@ export function activate(context: vscode.ExtensionContext) {
                 }
                 if (text === '') {
                     BackCount++; 
-                }
-                
-                
-            });
-            const disposable = vscode.commands.registerCommand('count', () => {
-
-                const seconds = (totalCursorTimeInMilliseconds).toFixed(2); // カーソルがエディタ上にあった時間を小数点以下2桁まで表示
-                vscode.window.showInformationMessage(`キーを ${keyCount} 回押しました。エンターキーを ${enter} 回押しました。バックスペースキーを ${BackCount} 回押しました。マウスカーソルがエディタ上にあった時間: ${seconds} 秒`);
-                console.log(name);
-
-                const datas = {
-                    "name": name,
-                    "keycount": keyCount,       
-                    "entercount": enter,
-                    "backcount": BackCount,
-                    "seconds": seconds,
-                };
-                sendToApi(datas);
-    
-                console.log(datas);
-    
-                async function sendToApi(datas: any): Promise<axios.AxiosResponse | undefined> {
-                    try {
-                        const response = await axios.post(url, datas, {
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                        });
-                        return response;
-                    } catch (error) {
-                        console.error(error);
-                    }
-    
-    
-                }
-                keyCount = 0;
-                BackCount = 0;
-                enter = 0;
-                totalCursorTimeInMilliseconds = 0;
-                
+                } 
             });
         }
-        
         else if(name === true){
             vscode.window.showInformationMessage(`名前を入力してください`);
-        }
-         else {
+        }else {
             vscode.window.showInformationMessage(`名前を入力してください`);
         }
+    }),
 
-        }),
 
-
+    // ここからは、、VSCodeのコマンドshowKeyCountの設定
         vscode.commands.registerCommand('vscode-Keys.showKeyCount', () => {
             if(name !== undefined){
             const seconds = (totalCursorTimeInMilliseconds).toFixed(2); // カーソルがエディタ上にあった時間を小数点以下2桁まで表示
@@ -220,12 +180,51 @@ export function activate(context: vscode.ExtensionContext) {
             enter = 0;
             totalCursorTimeInMilliseconds = 0;
 
-    
         }else{
             vscode.window.showInformationMessage(`名前を入力してください`); 
         }
-
         }),
-
     );
+
+
+// ここからは、、VSCodeのキーショットカットの設定
+    const disposable = vscode.commands.registerCommand('count', () => {
+        if(activ === true){
+        const seconds = (totalCursorTimeInMilliseconds).toFixed(2); // カーソルがエディタ上にあった時間を小数点以下2桁まで表示
+        vscode.window.showInformationMessage(`キーを ${keyCount} 回押しました。エンターキーを ${enter} 回押しました。バックスペースキーを ${BackCount} 回押しました。マウスカーソルがエディタ上にあった時間: ${seconds} 秒`);
+        console.log(name);
+
+        const datas = {
+            "name": name,
+            "keycount": keyCount,       
+            "entercount": enter,
+            "backcount": BackCount,
+            "seconds": seconds,
+        };
+        sendToApi(datas);
+
+        console.log(datas);
+
+        async function sendToApi(datas: any): Promise<axios.AxiosResponse | undefined> {
+            try {
+                const response = await axios.post(url, datas, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                return response;
+            } catch (error) {
+                console.error(error);
+            }
+
+
+        }
+        keyCount = 0;
+        BackCount = 0;
+        enter = 0;
+        totalCursorTimeInMilliseconds = 0;
+    }else{
+        vscode.window.showInformationMessage(`スタートを押してください`); 
+    }
+    });
 }
